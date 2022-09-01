88540138f5ce9ea2b2e7f1a82b78a8312c2f7232
Vitor Marcato <vitor.marcatof@gmail.com>
2022-08-31 21:24:45 -0300

    initial commit

---
+++ multiples3or5/MultiplesOf3Or5.java
@@ -0,0 +1,20 @@
+package multiples3or5;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiplesOf3Or5 {
+
+	public int solution(int number) {
+		
+		int multipleOfThree = number/3;
+		int multipleOfFive = number/5;
+		
+		List<Integer> list = new ArrayList<>();
+		
+		for (i; i<=multipleOfThree; i++) {
+			
+			
+		}
+				
+	  }
+}
