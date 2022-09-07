c87608b3cd0f4b6a98c63face89a90d2ea878bd8
Vitor Marcato <vitor.marcatof@gmail.com>
2022-09-07 09:18:22 -0300

    refactor

--- multiples3or5/MultiplesOf3Or5.java
+++ multiples3or5/MultiplesOf3Or5.java
@@ -1,53 +1,27 @@
 package multiples3or5;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class MultiplesOf3Or5 {
 
 	public static void main(String[] args) {
 
-		int number = 0;
-		int sum = 0;
-		
-		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please, enter a positive natual number: ");
-		number = Integer.parseInt(sc.nextLine());
+		System.out.println("Please, enter a positive natural number: ");
+		int number = Integer.parseInt(sc.nextLine());
 		sc.close();
-		
-		List<Integer> list = new ArrayList<>();
 
 		if (number > 0) {
-			int multipleOfThree = number / 3;
-			int multipleOfFive = number / 5;
-			for (int i = 1; i <= multipleOfThree; i++) {
-				int multiple = i * 3;
-				list.add(multiple);
-			}
 
-			for (int i = 1; i <= multipleOfFive; i++) {
-				int multiple = i * 5;
-				list.add(multiple);
-			}
-
-			// removing duplicates
-			List<Integer> newList = new ArrayList<Integer>();
-			for (Integer element : list) {
-
-				if (!newList.contains(element)) {
-
-					newList.add(element);
+			int sumThreeOrFive = 0;
+			
+			for (int i = 0; i <= number; i ++) {
+				if(i %3 == 0 || i %5 == 0){
+				sumThreeOrFive += i;
 				}
-
 			}
-
-			for (Integer numberOfNewList : newList) {
-				sum += numberOfNewList;
-			}
-
-			System.out.println(sum);
+			
+			System.out.println(sumThreeOrFive);
 
 		} else {
 			System.out.println(0);
